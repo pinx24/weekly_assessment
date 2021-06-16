35d56ebf18286f8cbe90976d90d251c452bf0acf
pinx24 <princeoputa@yahoo.com>
2021-06-16 17:11:13 +0100

    First commit

---
+++ src/main/kotlin/class.kt
@@ -0,0 +1,49 @@
+import java.lang.invoke.StringConcatFactory
+
+fun main (){
+    var goat =  ("white","silky", "long", "big", 20 )
+}
+
+con
+class `goat` {
+    var goatTeeth : String = ""
+    var goatFur : String = ""
+    var goatTail : String = ""
+    var goatEye : String = ""
+    var goatSetofteeth :Int =0
+
+}
+class man {
+    var manHeight: Double = 0.0
+    var manWeight: Double = 0.0
+    var manBeards: String = ""
+    var manGender: String = ""
+    var manPride: Boolean =
+
+
+}
+class woman{
+    var womanStatue : String =""
+    var womanCooking : String = ""
+    var womanFunny : String = ""
+    var womanTribe : String = ""
+    var womanHeight : String = ""
+
+}
+class sheep{
+    var sheepFur : String = ""
+    var sheepTeeth : String = ""
+    var sheepTongue : String = ""
+    var sheepObedient : String = ""
+    var sheepSales : String = ""
+
+}
+class business{
+    var businessRisk : String = ""
+    var businessCapital : String = ""
+    var businessWorkers : String = ""
+    var businessFailure : String = ""
+    var businessEthics : String = " "
+}
+
+constructor ( var  goatTeeth: String, goatFur: String,goatTail: String, goatEye:String , goatsetofTeeth: String),
---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,113 @@
+fun main() {
+    var age = 24
+    if (age >=18){
+        println("Welcome")
+    }
+     val userName = "John"
+    if (userName == "John"){
+        println(userName)
+    }
+    var number =arrayOf(1,2,3,5,6,7,8)
+    if(number.size== 3){
+        println(number)
+
+
+        }
+
+    }
+fun main(){
+    val age = 21
+    if (age>= 20)
+
+}
+fun main(){
+val num= 20
+if (num<=16){
+    println("Goodmorning ${num}"
+   }
+}
+fun  main (){
+    val age = 21
+    if (age>= 22 ){
+
+println (age)
+
+}
+    }
+fun (time){
+    in 0..11 -> println("Goodmorning!")
+    12 -> println ("Time for Lunch!")
+    else -> println("Good day!")
+
+ }
+
+var name:Int =
+for(i in 1..100){
+    println(i)}
+
+    var name: Int =
+       for(a in 1..10 && !4..5){
+           println(a)
+       }
+
+           var sum = 0
+           var i = 10
+           while (i <= 20){
+    i++
+    if (i % 2 != 0) {
+        continue
+    }
+    sum += i
+}
+println(sum}
+
+           }           }
+
+
+fun main (){
+    var goat =  ("white","silky", "long", "big", 20 )
+}
+
+con
+class `goat` {
+    var goatTeeth : String = ""
+    var goatFur : String = ""
+    var goatTail : String = ""
+    var goatEye : String = ""
+    var goatSetofteeth :Int =0
+
+}
+class man {
+    var manHeight: Double = 0.0
+    var manWeight: Double = 0.0
+    var manBeards: String = ""
+    var manGender: String = ""
+    var manPride: Boolean =
+
+
+}
+class woman{
+    var womanStatue : String =""
+    var womanCooking : String = ""
+    var womanFunny : String = ""
+    var womanTribe : String = ""
+    var womanHeight : String = ""
+
+}
+class sheep{
+    var sheepFur : String = ""
+    var sheepTeeth : String = ""
+    var sheepTongue : String = ""
+    var sheepObedient : String = ""
+    var sheepSales : String = ""
+
+}
+class business{
+    var businessRisk : String = ""
+    var businessCapital : String = ""
+    var businessWorkers : String = ""
+    var businessFailure : String = ""
+    var businessEthics : String = " "
+}
+
+constructor ( var  goatTeeth: String, goatFur: String,goatTail: String, goatEye:String , goatsetofTeeth: String),
